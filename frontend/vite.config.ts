3c97a25a0baf01018ab759d7ca156346199c7472
Gianmichele Siano <gianmichele.siano@ngft.com>
2025-12-18 17:36:57 +0100

    back to defoult port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
